2d5f52997b6fbb6e0317cccf6dc736c3d7bf71aa
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2021-09-29 23:29:22 +0300

    Update EnsureInputTests.java

--- app/src/androidTest/java/com/sample/browserstack/samplecalculator/EnsureInputTests.java
+++ app/src/androidTest/java/com/sample/browserstack/samplecalculator/EnsureInputTests.java
@@ -1,56 +1,56 @@
-package com.sample.browserstack.samplecalculator;
+// package com.sample.browserstack.samplecalculator;
 
-import androidx.test.filters.SmallTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+// import androidx.test.filters.SmallTest;
+// import androidx.test.rule.ActivityTestRule;
+// import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.squareup.spoon.Spoon;
+// import com.squareup.spoon.Spoon;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+// import org.junit.Before;
+// import org.junit.Rule;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+// import static androidx.test.espresso.Espresso.onView;
+// import static androidx.test.espresso.action.ViewActions.click;
+// import static androidx.test.espresso.assertion.ViewAssertions.matches;
+// import static androidx.test.espresso.matcher.ViewMatchers.withId;
+// import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-/**
- * Espresso tests to ensure that editText box is updated appropriately
- * whenever buttons are clicked
- */
+// /**
+//  * Espresso tests to ensure that editText box is updated appropriately
+//  * whenever buttons are clicked
+//  */
 
-@SmallTest
-@RunWith(AndroidJUnit4.class)
-public class EnsureInputTests {
+// @SmallTest
+// @RunWith(AndroidJUnit4.class)
+// public class EnsureInputTests {
 
-    @Rule
-    public ActivityTestRule<MainActivity> activityRule =
-            new ActivityTestRule<MainActivity>(MainActivity.class);
+//     @Rule
+//     public ActivityTestRule<MainActivity> activityRule =
+//             new ActivityTestRule<MainActivity>(MainActivity.class);
 
-    private MainActivity mainActivity;
+//     private MainActivity mainActivity;
 
-    @Before
-    public void setUp() {
-        mainActivity = activityRule.getActivity();
-    }
+//     @Before
+//     public void setUp() {
+//         mainActivity = activityRule.getActivity();
+//     }
 
-    @Test
-    public void ensureSingleInputIsHandled() {
-        Spoon.screenshot(mainActivity, "initial_state");
-        onView(withId(R.id.buttonOne)).perform(click());
-        onView(withId(R.id.editText)).check(matches(withText("1")));
-        Spoon.screenshot(mainActivity, "post_multiple_btn_click");
-    }
+//     @Test
+//     public void ensureSingleInputIsHandled() {
+//         Spoon.screenshot(mainActivity, "initial_state");
+//         onView(withId(R.id.buttonOne)).perform(click());
+//         onView(withId(R.id.editText)).check(matches(withText("1")));
+//         Spoon.screenshot(mainActivity, "post_multiple_btn_click");
+//     }
 
-    @Test
-    public void ensureMultipleInputIsHandled() {
-        Spoon.screenshot(mainActivity, "initial_state");
-        onView(withId(R.id.buttonOne)).perform(click());
-        onView(withId(R.id.buttonTwo)).perform(click());
-        onView(withId(R.id.editText)).check(matches(withText("12")));
-        Spoon.screenshot(mainActivity, "post_multiple_btn_click");
-    }
-}
+//     @Test
+//     public void ensureMultipleInputIsHandled() {
+//         Spoon.screenshot(mainActivity, "initial_state");
+//         onView(withId(R.id.buttonOne)).perform(click());
+//         onView(withId(R.id.buttonTwo)).perform(click());
+//         onView(withId(R.id.editText)).check(matches(withText("12")));
+//         Spoon.screenshot(mainActivity, "post_multiple_btn_click");
+//     }
+// }
